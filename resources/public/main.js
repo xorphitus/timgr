4768a59dfb5c666967172f1eb664d6c60fd964e4
xorphitus <xorphitus@gmail.com>
2013-10-11 02:16:47 +0900

    時間集計画面のモックを作成

--- resources/public/main.js
+++ resources/public/main.js
@@ -25,3 +25,44 @@ var $calendar = $('#calendar').fullCalendar({
     },
     editable: true
 });
+
+
+$('.datepicker').datepicker();
+
+$('#go').on('click', function () {
+    $('#result').removeClass('hide');
+
+    $.jqplot('graph1', [
+        [[2,1], [4,2], [6,3], [3,4]],
+        [[5,1], [1,2], [3,3], [4,4]],
+        [[4,1], [7,2], [1,3], [2,4]]], {
+            seriesDefaults: {
+                renderer:$.jqplot.BarRenderer,
+                pointLabels: { show: true, location: 'e', edgeTolerance: -15 },
+                shadowAngle: 135,
+                rendererOptions: {
+                    barDirection: 'horizontal'
+                }
+            },
+            axes: {
+                yaxis: {
+                    renderer: $.jqplot.CategoryAxisRenderer
+                }
+            }
+        });
+
+    $.jqplot ('graph2', [[
+        ['Heavy Industry', 12],['Retail', 9], ['Light Industry', 14],
+        ['Out of home', 16],['Commuting', 7], ['Orientation', 9]
+    ]],
+              {
+                  seriesDefaults: {
+                      renderer: $.jqplot.PieRenderer,
+                      rendererOptions: {
+                          showDataLabels: true
+                      }
+                  },
+                  legend: { show:true, location: 'e' }
+              }
+             );
+});
--- src/timgr/handler.clj
+++ src/timgr/handler.clj
@@ -5,7 +5,7 @@
             [hiccup.core :as hiccup]))
 
 (defn layout [title content]
-  (html
+  (hiccup/html
    (hiccup.page/doctype :html5)
    [:html {:lang "ja"}
     [:head
@@ -15,6 +15,7 @@
      [:link {:rel "stylesheet" :href "//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.css"}]
      [:link {:rel "stylesheet" :href "//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.print.css" :media "print"}]
      [:link {:rel "stylesheet" :href "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"}]
+     [:link {:rel "stylesheet" :href "//cdn.jsdelivr.net/jqplot/1.0.8/jquery.jqplot.css"}]
      [:link {:rel "stylesheet" :href "/main.css"}]]
     [:body
      [:div.navbar.navbar-inverse.navbar-fixed-top
@@ -24,17 +25,42 @@
        [:div.collapse.navbar-collapse
         [:ul.nav.navbar-nav
          [:li.active [:a {:href "/"} "time recording"]]
-         [:li [:a {:href "#"} "statistics"]]
+         [:li [:a {:href "/statistics"} "statistics"]]
          ]]]]
      [:div#main-content.container content]
      [:script {:src "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"}]
      [:script {:src "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"}]
      [:script {:src "//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"}]
      [:script {:src "//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.min.js"}]
+     [:script {:src "//cdn.jsdelivr.net/jqplot/1.0.8/jquery.jqplot.js"}]
+     [:script {:src "//cdn.jsdelivr.net/jqplot/1.0.8/plugins/jqplot.barRenderer.min.js"}]
+     [:script {:src "//cdn.jsdelivr.net/jqplot/1.0.8/plugins/jqplot.categoryAxisRenderer.min.js"}]
+     [:script {:src "//cdn.jsdelivr.net/jqplot/1.0.8/plugins/jqplot.pointLabels.min.js"}]
+     [:script {:src "//cdn.jsdelivr.net/jqplot/1.0.8//plugins/jqplot.pieRenderer.min.js"}]
      [:script {:src "/main.js"}]]]))
 
+(defn statistics-content []
+     [:div#statistics
+      [:form.form-inline {:role "form"}
+       [:div.form-group
+        [:label.sr-only "date from"]
+        [:input.form-control.datepicker {:type "text" :placeholder "date from"}]]
+       "&nbsp;~&nbsp;"
+       [:div.form-group
+        [:label.sr-only "date to"]
+        [:input.form-control.datepicker {:type "text" :placeholder "date to"}]]
+       "&nbsp;"
+       [:a#go.btn.btn-default {:href "#"} "Go"]
+       ]
+      [:div#result.hide
+       [:h3 "accumulation"]
+       [:div#graph1]
+       [:h3 "ratio"]
+       [:div#graph2]]])
+
 (defroutes app-routes
-  (GET "/" [] (layout "main page" [:div#calendar]))
+  (GET "/" [] (layout "time recording" [:div#calendar]))
+  (GET "/statistics" [] (layout "statistics" (statistics-content)))
   (route/resources "/")
   (route/not-found "Not Found"))
 
